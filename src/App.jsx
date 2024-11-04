fbea90b5563b71aef6070724e31694a7d250a626
Akarsha19 <akarshap19@gmail.com>
2024-11-04 01:01:09 -0500

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import './App.css';
+import './components';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
   return (
     <>
-      <div>
-
-      </div>
+      <ContactList />
 
     </>
   )
 }
 
-export default App
+
